629253ae4b33a81760e941a96cc7f73056b3a8b6
theDeji <emmi2110@gmail.com>
2021-01-07 03:58:21 -1200

    new commit

---
+++ build/precache-manifest.960147e6e7aa81f614aeaea57fba552f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda3845accc0c3dd159ba122ffc52cfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d367c3b71a110852490a",
+    "url": "/static/css/main.24e6a2f3.chunk.css"
+  },
+  {
+    "revision": "91246862a7fdf3f2370a",
+    "url": "/static/js/2.57fd2a7b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.57fd2a7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d367c3b71a110852490a",
+    "url": "/static/js/main.65d4333e.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "9169bfd40e8b280cc0a5d1c3677efcd6",
+    "url": "/static/media/bg.9169bfd4.png"
+  }
+]);
